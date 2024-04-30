3e234472da500dd94469986b7379dcd8b9c31d02
Jupi <spacejupiter0@gmail.com>
2024-04-30 12:05:43 +0200

    undid some change

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import "./index.css";
-import { ClerkProvider } from "@clerk/clerk-react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
 
-const PUBLISHABLE_KEY =
-  "pk_test_ZXhjaXRpbmcta29hbGEtNDUuY2xlcmsuYWNjb3VudHMuZGV2JA";
+const queryClient = new QueryClient();
 
-if (!PUBLISHABLE_KEY) {
-  throw new Error("Missing Publishable Key");
+// Import the generated route tree
+import { routeTree } from "./routeTree.gen";
+
+// Create a new router instance
+const router = createRouter({ routeTree });
+
+// Register the router instance for type safety
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
 }
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-      <QueryClientProvider client={queryClient}>
-        <RouterProvider router={router} />
-      </QueryClientProvider>
-    </ClerkProvider>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>
 );
