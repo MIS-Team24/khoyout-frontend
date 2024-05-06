7bc15c2bd3d859a2dba160d6818de46b9389f362
AmrAbouElkair <amrmahmoud20022002@gmail.com>
2024-05-07 01:05:12 +0300

    Add redux-persist dependency

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,43 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  PersistConfig,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import usersReducer from "./features/users";
 
-export const store = configureStore({
-  reducer: {
-    users: usersReducer,
-  },
+const persistConfig: PersistConfig<RootState> = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const rootReducer = combineReducers({
+  users: usersReducer,
 });
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
+
 // Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>;
+export type RootState = ReturnType<typeof rootReducer>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
