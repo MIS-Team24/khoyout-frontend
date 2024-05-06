8064db507bfe24ea8bb5062a7a6f2f66044f188a
AmrAbouElkair <amrmahmoud20022002@gmail.com>
2024-05-07 01:06:10 +0300

    Add verifyEmail function to register API module

--- src/API/register/register.ts
+++ src/API/register/register.ts
@@ -13,3 +13,13 @@ export function register(data: API_RegisterBody) {
     repeatPassword: data.confirmPassword,
   });
 }
+
+type API_VerifyEmailBody = {
+  email: string;
+  keyVal: string;
+  code: string;
+};
+
+export function verifyEmail(data: API_VerifyEmailBody) {
+  return client.post("/auth/verify-email", data);
+}
