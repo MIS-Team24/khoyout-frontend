5d1cc5d4aaf7ca5550946bf76f844d4cdbf7d839
ammarelshrief <amarelshrief511@gmail.com>
2024-05-07 19:36:11 +0300

    Install Form and Label components from shadcn ui

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,2 +1,12 @@
 export { Button, buttonVariants } from "./button";
 export { Input } from "./input";
+export { Label } from "./label";
+export {
+  Form,
+  FormField,
+  FormItem,
+  FormDescription,
+  FormControl,
+  FormLabel,
+  FormMessage,
+} from "./form";
