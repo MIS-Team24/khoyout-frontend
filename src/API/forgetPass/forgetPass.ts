20f67d369d5fecc0e0a69e471064e83efe419431
AmrAbouElkair <amrmahmoud20022002@gmail.com>
2024-05-08 20:57:43 +0300

    Remove forgetPass API function

--- src/API/forgetPass/forgetPass.ts
+++
@@ -1,9 +0,0 @@
-import client from "@/API/client";
-
-type API_ForgetPassBody = {
-  email: string;
-};
-
-export function forgetPass(data: API_ForgetPassBody) {
-  return client.post("auth/otp", data);
-}
