eb7b8cd69d40a543cd57011ba78080eab3a1d4a6
Jupi <spacejupiter0@gmail.com>
2024-05-11 14:18:53 +0300

    created designer profile view

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,10 +1,21 @@
+import DesignerProfileCard from "@/components/views/designers/DesignerProfileCard";
+
 export default function Home() {
   return (
     <div>
-      This is the main home page gjiaegjapgjasopge Lorem ipsum dolor sit amet
-      consectetur adipisicing elit. Quis veritatis aspernatur eum doloribus
-      provident minus blanditiis harum, obcaecati eveniet totam nulla deleniti
-      fuga deserunt ducimus ipsum quo est velit? Soluta.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi nulla
+      itaque molestiae ullam blanditiis neque quos sint aperiam omnis! Iste
+      minima laborum voluptatum quos voluptatibus pariatur vitae atque obcaecati
+      dolorem.
+      <DesignerProfileCard
+        id="damsodR23Dmoa"
+        name="Jupi Juuu"
+        address={{ city: "Alexandria", province: "Asafra" }}
+        ratings={{ average: 2.5, totalCount: 120 }}
+        wishlisted={false}
+        yearsOfExperienceCount={5}
+        key={"asplflaspf"}
+      />
     </div>
   );
 }
