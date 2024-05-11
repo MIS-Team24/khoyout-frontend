4d4ed8eb0768b9d25e9773780cf449feeefd9ab1
AmrAbouElkair <amrmahmoud20022002@gmail.com>
2024-05-11 21:44:51 +0300

    Update section margin in Inspiration component

--- src/components/views/home/inspiration/Inspiration.tsx
+++ src/components/views/home/inspiration/Inspiration.tsx
@@ -10,9 +10,11 @@ import { ChevronRight } from "lucide-react";
 import { useRef } from "react";
 import Autoplay from "embla-carousel-autoplay";
 import { inspirationImages } from "@/assets";
+import { Link } from "@tanstack/react-router";
+import { motion } from "framer-motion";
 
 export default function Inspiration() {
-  const autoplay = useRef(Autoplay({ delay: 2000, stopOnInteraction: true }));
+  const autoplay = useRef(Autoplay({ delay: 100000, stopOnInteraction: true }));
 
   return (
     <section className="my-[5rem]">
@@ -39,13 +41,29 @@ export default function Inspiration() {
               {inspirationImages.map(({ alt, src }, i) => (
                 <CarouselItem
                   key={`inspiration-image-${i}`}
-                  className="h-[27.8rem] w-[23rem] basis-full rounded-[0.5rem] md:basis-1/2 lg:basis-1/3 2xl:basis-1/4"
+                  className="h-[27.8rem] w-[23rem] basis-full overflow-hidden rounded-[0.5rem] md:basis-1/2 lg:basis-1/3 2xl:basis-1/4"
                 >
-                  <img
-                    src={src}
-                    alt={alt}
-                    className="h-full w-full rounded-[0.5rem] object-cover"
-                  />
+                  <div className="group relative">
+                    <img
+                      src={src}
+                      alt={alt}
+                      className="h-full w-full rounded-[0.5rem] object-cover"
+                    />
+                    <motion.span
+                      initial={{ opacity: 0 }}
+                      whileHover={{ opacity: 1 }}
+                      transition={{ duration: 0.2 }}
+                      className="absolute inset-0 z-50 hidden items-end justify-center space-x-1 rounded-[0.5rem] bg-gradient-to-t from-primary/70 to-transparent pb-4 text-[1.3rem] leading-[2rem] text-white group-hover:flex"
+                    >
+                      <span>Designed by</span>
+                      <Link
+                        to="/"
+                        className="cursor-pointer font-medium hover:underline"
+                      >
+                        Bassma Adel
+                      </Link>
+                    </motion.span>
+                  </div>
                 </CarouselItem>
               ))}
             </CarouselContent>
