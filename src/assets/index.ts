edabc6b2ffcabfbc9bd26a2f3597e7d75ff4f285
AmrAbouElkair <amrmahmoud20022002@gmail.com>
2024-05-11 18:42:38 +0300

    Add Inspiration images

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,4 +1,43 @@
 import sideImg from "./side-img.webp";
 import mainLogo from "./Logo.svg";
+import heroImg from "./hero.png";
+import ins1 from "./Ins1.png";
+import ins2 from "./Ins2.png";
+import ins3 from "./Ins3.png";
+import ins4 from "./Ins4.png";
+import ins5 from "./Ins5.png";
+import ins6 from "./Ins6.png";
+import ins8 from "./Ins8.png";
 
-export { sideImg, mainLogo };
+const inspirationImages = [
+  {
+    src: ins1,
+    alt: "inspiration-image-1",
+  },
+  {
+    src: ins2,
+    alt: "inspiration-image-2",
+  },
+  {
+    src: ins3,
+    alt: "inspiration-image-3",
+  },
+  {
+    src: ins4,
+    alt: "inspiration-image-4",
+  },
+  {
+    src: ins5,
+    alt: "inspiration-image-5",
+  },
+  {
+    src: ins6,
+    alt: "inspiration-image-6",
+  },
+  {
+    src: ins8,
+    alt: "inspiration-image-8",
+  },
+];
+
+export { sideImg, mainLogo, heroImg, inspirationImages };
