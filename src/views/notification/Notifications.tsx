78aa0070a184743312a8df3e8808aea591bd6788
ammarelshrief <amarelshrief511@gmail.com>
2024-05-12 23:40:07 +0300

    Add Notifications page

---
+++ src/views/notification/Notifications.tsx
@@ -0,0 +1,10 @@
+import NavigatablePageWrapper from "@/components/custom/NavigatablePageWrapper";
+import { Notification } from "@/components/views/notification";
+
+export default function Notifications() {
+  return (
+    <NavigatablePageWrapper>
+      <Notification />
+    </NavigatablePageWrapper>
+  );
+}
