72067d37b771af3eae134f5f87a2573377b692f0
AmrAbouElkair <amrmahmoud20022002@gmail.com>
2024-05-13 02:04:46 +0300

    Add AboutMap component to display designer location on profile page

---
+++ src/components/views/designerProfile/aboutDesigner/AboutMap.tsx
@@ -0,0 +1,32 @@
+import { MapContainer, TileLayer, Marker } from "react-leaflet";
+import { Icon } from "leaflet";
+import { markerImg, markerShadow } from "@/assets";
+
+const legalIcon = new Icon({
+  iconUrl: markerImg,
+  shadowUrl: markerShadow,
+  shadowSize: [40, 40],
+  iconSize: [40, 40],
+  iconAnchor: [18, 38],
+  shadowAnchor: [12, 40],
+  popupAnchor: [0, -40],
+});
+
+const position: [number, number] = [31.214457, 29.994616];
+
+export default function AboutMap() {
+  return (
+    <MapContainer
+      className="h-full w-full rounded-[0.5rem]"
+      center={position}
+      zoom={13}
+      scrollWheelZoom={true}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={position} icon={legalIcon} />
+    </MapContainer>
+  );
+}
