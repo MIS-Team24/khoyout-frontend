54258d90afaddeede05788ccf7937600f21639dd
ammarelshrief <amarelshrief511@gmail.com>
2024-05-12 04:28:03 +0300

    Add Client and Designer images and export them

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,4 +1,23 @@
 import sideImg from "./side-img.webp";
 import mainLogo from "./Logo.svg";
+import scratchImg from "./scratchImg.jpeg";
+import implementImg from "./implementImg.jpeg";
+import redesignImg from "./redesignImg.png";
+import handImg from "./handImg.jpeg";
+import quotes from "./Quotes.svg";
+import amira from "./amira.jpeg";
+import hager from "./hager.jpeg";
+import may from "./may.jpeg";
 
-export { sideImg, mainLogo };
+export {
+  sideImg,
+  mainLogo,
+  scratchImg,
+  implementImg,
+  redesignImg,
+  handImg,
+  quotes,
+  amira,
+  hager,
+  may,
+};
