98d62fcf3254cc38f28d6d8c3d0220238af8c47b
AmrAbouElkair <amrmahmoud20022002@gmail.com>
2024-05-12 17:18:14 +0300

    Add navigation wrapper to DesignerProfile page

--- src/views/designerProfile/DesignerProfile.tsx
+++ src/views/designerProfile/DesignerProfile.tsx
@@ -1,5 +1,10 @@
+import NavigatablePageWrapper from "@/components/custom/NavigatablePageWrapper";
 import { Test } from "@/components/views/designerProfile";
 
 export default function DesignerProfile() {
-  return <Test />;
+  return (
+    <NavigatablePageWrapper>
+      <Test />
+    </NavigatablePageWrapper>
+  );
 }
