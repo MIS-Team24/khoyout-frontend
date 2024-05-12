a236ebccd3cdcf64494a209a176cd1947519273d
ammarelshrief <amarelshrief511@gmail.com>
2024-05-12 04:30:23 +0300

    Add clients and designers components to home page

---
+++ src/components/views/home/index.ts
@@ -0,0 +1,2 @@
+export { default as Designers } from "./Designers";
+export { default as Clients } from "./Clients";
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,10 +1,10 @@
+import { Designers, Clients } from "@/components/views/home";
+
 export default function Home() {
   return (
-    <div>
-      This is the main home page gjiaegjapgjasopge Lorem ipsum dolor sit amet
-      consectetur adipisicing elit. Quis veritatis aspernatur eum doloribus
-      provident minus blanditiis harum, obcaecati eveniet totam nulla deleniti
-      fuga deserunt ducimus ipsum quo est velit? Soluta.
-    </div>
+    <main>
+      <Designers />
+      <Clients />
+    </main>
   );
 }
