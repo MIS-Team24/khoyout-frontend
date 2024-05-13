87b93b00f4fd145adf29bbff36990332425073cb
AmrAbouElkair <amrmahmoud20022002@gmail.com>
2024-05-13 14:32:53 +0300

    Add DesignerVideos component to DesignerProfile

--- src/components/views/designerProfile/index.ts
+++ src/components/views/designerProfile/index.ts
@@ -1,2 +1,3 @@
 export { default as ProfileDetails } from "./profileDetails/ProfileDetails";
 export { default as AboutDesigner } from "./aboutDesigner/AboutDesigner";
+export { default as DesignerVideos } from "./designerVideos/DesignerVideos";
