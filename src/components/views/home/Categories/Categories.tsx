b77da8b208d457141f2af189ef272998f14a3eb1
ammarelshrief <amarelshrief511@gmail.com>
2024-05-13 22:29:05 +0300

    Create categories component and add it to home page

--- src/components/views/home/Categories/Categories.tsx
+++ src/components/views/home/Categories/Categories.tsx
@@ -1,40 +1,26 @@
-import { soiree, classic, formal, casual } from "@/assets";
+import { categories } from "@/assets";
 
 function Categories() {
   return (
     <section className="mt-16">
       <div className="mx-auto flex w-full items-center justify-center">
-        <div className="inline-flex flex-col items-center ">
-          <div className="w-full ">
-            <h2 className="text-[2rem] font-normal text-[#1F1F29]">
-              Browse Designers By Categories
-            </h2>
-          </div>
-          <div className="m-6 grid h-full w-full grid-cols-2 ">
-            <div className=" ">
-              <img
-                src={soiree}
-                alt="Soiree Image"
-                className=" mb-6 h-[19.8125rem] w-full max-w-[35.25rem] rounded-tl-[.5rem]"
-              />
-              <img
-                src={casual}
-                alt="Casual Image"
-                className=" h-[19.8125rem] w-[35.25rem] rounded-bl-[.5rem]"
-              />
-            </div>
-            <div className="grid w-full">
-              <img
-                src={classic}
-                alt="Classic Image"
-                className=" mb-6 h-[19.8125rem] w-[35.25rem] rounded-tr-[.5rem]"
-              />
-              <img
-                src={formal}
-                alt="Formal Image"
-                className=" h-[19.8125rem] w-[35.25rem] rounded-br-[.5rem]"
-              />
-            </div>
+        <div className="flex w-full flex-col items-center lg:w-[72rem]">
+          <h2 className="w-full pb-8 text-[2rem] font-normal">
+            Browse Designers By Categories
+          </h2>
+          <div className="flex w-full flex-wrap justify-center gap-6 px-4 sm:px-0">
+            {categories.map(({ src, alt }, i) => (
+              <div
+                className="h-[19.9rem] w-full max-w-[35.25rem] rounded-[0.5rem]"
+                key={i}
+              >
+                <img
+                  src={src}
+                  alt={alt}
+                  className="h-full w-full rounded-[0.5rem] object-cover"
+                />
+              </div>
+            ))}
           </div>
         </div>
       </div>
