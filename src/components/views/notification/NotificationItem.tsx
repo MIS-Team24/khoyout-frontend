7f20b32844e5888f4d476376ebdca36a24f0eb9c
ammarelshrief <amarelshrief511@gmail.com>
2024-05-13 22:28:25 +0300

    Fix responsive "NotificationIte,.tsx"

--- src/components/views/notification/NotificationItem.tsx
+++ src/components/views/notification/NotificationItem.tsx
@@ -10,15 +10,15 @@ export default function NotificationItem(props: {
     <div className="mb-12">
       <div className="flex w-full items-center justify-between px-4 pb-8">
         <div className="flex items-center justify-center">
-          <div className="h-[4.25rem] w-[6.375rem]">
+          <div className="w-[8rem] sm:h-[4.25rem] sm:w-[6.375rem]">
             <img
               src={props.image}
               alt="Client Image"
               className="h-[5rem] w-[5rem] rounded-[50%] object-cover"
             />
           </div>
-          <p className="text-2xl font-normal text-[#1F1F29]">
-            <span className="text-2xl font-semibold text-[#8C236C]">
+          <p className="ml-4 text-xl font-normal text-[#1F1F29]">
+            <span className="text-xl font-semibold text-[#8C236C]">
               {props.name}
             </span>{" "}
             {props.describtion}
