0ffe5304c38eb9de0cd0d6cc202d9daf83b4f2a8
yehia <3202114888@alexu.edu.eg>
2024-05-13 19:25:10 +0300

    upload login page

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,2 +1,8 @@
-export { Button, buttonVariants } from "./button";
-export { Input } from "./input";
+export {Button, buttonVariants} from "./button";
+export {Input} from "./input";
+export {
+    Form, FormControl, FormDescription, FormField,
+    FormItem,
+    FormLabel,
+    FormMessage
+} from "./form";
---
+++ src/views/loginPage.tsx
@@ -0,0 +1,7 @@
+import LoginHomePage from "@/components/views/login/LoginHomePage.tsx";
+
+export default function LoginPage() {
+    return (
+        <LoginHomePage/>
+    );
+}
