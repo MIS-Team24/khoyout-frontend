c47d377721f0b0c341bf3027ada8a533e7226bdc
AmrAbouElkair <amrmahmoud20022002@gmail.com>
2024-05-14 19:07:39 +0300

    Add SegmentationSteps and SegmentStep components

--- src/components/custom/index.ts
+++ src/components/custom/index.ts
@@ -2,3 +2,5 @@ export { default as LoadingState } from "./Loading";
 export { default as NavigatablePageWrapper } from "./NavigatablePageWrapper";
 export { default as Segment } from "./Segmentation/Segment";
 export { default as Segmentator } from "./Segmentation/Segmentator";
+export { default as SegementationSteps } from "./Segmentation/SegementationSteps";
+export { default as SegmentStep } from "./Segmentation/SegmentStep";
