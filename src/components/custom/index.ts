62eda0beffdcd30f797ecf4e829f4164559a2ca9
AmrAbouElkair <amrmahmoud20022002@gmail.com>
2024-05-14 19:01:15 +0300

    Add Segment and Segmentator components

--- src/components/custom/index.ts
+++ src/components/custom/index.ts
@@ -1,2 +1,4 @@
 export { default as LoadingState } from "./Loading";
 export { default as NavigatablePageWrapper } from "./NavigatablePageWrapper";
+export { default as Segment } from "./Segmentation/Segment";
+export { default as Segmentator } from "./Segmentation/Segmentator";
