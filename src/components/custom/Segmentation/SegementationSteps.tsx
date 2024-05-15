25afe47102a4eb93aed8baf21ad10bb0622a8026
AmrAbouElkair <amrmahmoud20022002@gmail.com>
2024-05-15 22:36:09 +0300

    Update SegmentationSteps component

--- src/components/custom/Segmentation/SegementationSteps.tsx
+++ src/components/custom/Segmentation/SegementationSteps.tsx
@@ -1,6 +1,7 @@
-import { Clamp } from "@/utilities/clamp";
-import { AnimatePresence, Variants, motion } from "framer-motion";
+import { Variants, motion } from "framer-motion";
 import { Children } from "react";
+import { circle, circleCheck, circleDot } from "@/assets";
+import { cn } from "@/lib/utils";
 
 type SegementatorProps = {
   children: React.ReactNode;
@@ -12,27 +13,61 @@ const animationVariants: Variants = {
   active: { scaleX: 1, transition: { ease: "linear" } },
 };
 
-export function SegementationSteps(props: SegementatorProps) {
-  const steps = Children.toArray(props.children);
+export function SegementationSteps({
+  children,
+  currentStep,
+}: SegementatorProps) {
+  const steps = Children.toArray(children);
 
   return (
-    <div className="gap-21 flex w-full flex-col p-0">
-      <div>
-        <AnimatePresence mode="wait">
-          {steps[Clamp(props.currentStep, 0, steps.length)]}
-        </AnimatePresence>
-      </div>
-      <div className="flex w-full gap-1">
-        {steps.map((_, index) => (
-          <div className="relative h-1 w-full bg-gray-400" key={index}>
-            <motion.div
-              className="absolute left-0 top-0 h-full w-full bg-primary"
-              variants={animationVariants}
-              animate={props.currentStep >= index ? "active" : "inactive"}
-              initial={{ scaleX: 0, transformOrigin: "left center" }}
-            ></motion.div>
-          </div>
-        ))}
+    <div className="flex w-full max-w-[18rem]">
+      <div className="flex w-full">
+        {steps.map(
+          (_, i) =>
+            i < steps.length - 1 && (
+              <div
+                key={i}
+                className={cn(
+                  "mx-auto flex items-center",
+                  i < steps.length - 2 ? "w-full" : "w-full max-w-fit",
+                )}
+              >
+                {i < steps.length - 1 && (
+                  <div className="min-w-fit">
+                    {currentStep > i ? (
+                      <img
+                        src={circleCheck}
+                        alt="circle-check"
+                        className="h-[1.75rem] w-[1.75rem] object-cover"
+                      />
+                    ) : currentStep === i ? (
+                      <img
+                        src={circleDot}
+                        alt="circle-dot"
+                        className="h-[1.75rem] w-[1.75rem] object-cover"
+                      />
+                    ) : (
+                      <img
+                        src={circle}
+                        alt="circle"
+                        className="h-[1.75rem] w-[1.75rem] object-cover"
+                      />
+                    )}
+                  </div>
+                )}
+                {i < steps.length - 2 && (
+                  <div className="relative mx-2 h-1 w-full rounded-lg bg-gray-400">
+                    <motion.div
+                      className="absolute left-0 top-0 h-full w-full rounded-lg bg-primary"
+                      variants={animationVariants}
+                      animate={currentStep > i ? "active" : "inactive"}
+                      initial={{ scaleX: 0, transformOrigin: "left center" }}
+                    ></motion.div>
+                  </div>
+                )}
+              </div>
+            ),
+        )}
       </div>
     </div>
   );
