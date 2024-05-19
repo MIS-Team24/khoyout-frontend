00780a55f92f2906cb69e109f7aa40b246d48ffe
AmrAbouElkair <amrmahmoud20022002@gmail.com>
2024-05-20 01:07:23 +0300

    Remove Unused Images

--- src/routes/images.lazy.tsx
+++
@@ -1,6 +0,0 @@
-import ImagePagination from "@/views/imagesPagination/ImagePagination";
-import { createLazyFileRoute } from "@tanstack/react-router";
-
-export const Route = createLazyFileRoute("/images")({
-  component: ImagePagination,
-});
