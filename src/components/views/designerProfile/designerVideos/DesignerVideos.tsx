3e350dffbb9162fdc90674bab2419294258603f9
AmrAbouElkair <amrmahmoud20022002@gmail.com>
2024-05-21 21:05:16 +0300

    Update DesignerVideos component layout and styles

--- src/components/views/designerProfile/designerVideos/DesignerVideos.tsx
+++ src/components/views/designerProfile/designerVideos/DesignerVideos.tsx
@@ -4,43 +4,41 @@ import { designerVideo1, designerVideo2 } from "@/assets";
 
 export default function DesignerVideos() {
   return (
-    <section className="my-[5rem]">
-      <div className="mx-auto min-h-[34rem] w-full max-w-[90rem] px-4">
-        <div className="flex w-full items-center justify-between">
-          <h2 className="text-[1.3rem] font-normal sm:text-[1.5rem] md:text-[2rem]">
-            Videos
-          </h2>
-          <Button
-            className="flex items-center gap-x-1 text-base font-medium leading-normal text-primary hover:bg-transparent hover:text-primary md:text-[1.2rem]"
-            variant="ghost"
-          >
-            See more
-            <ChevronRight size={25} />
-          </Button>
-        </div>
-        <div className="flex flex-col items-center justify-center gap-8 pt-8 lg:flex-row">
-          <div className="group relative h-full flex-1 cursor-pointer rounded-[0.25rem]">
-            <img
-              src={designerVideo1}
-              alt="designer-video-1"
-              className="h-full w-full rounded-[0.25rem] object-cover"
-            />
-            <div className="absolute inset-0 flex items-center justify-center rounded-[0.25rem] bg-black/30">
-              <div className="flex h-[4.25rem] w-[4.25rem] items-center justify-center rounded-full bg-primary group-hover:opacity-80">
-                <Play className="fill-white text-white" size={35} />
-              </div>
+    <section className="main-container mt-16">
+      <div className="flex w-full items-center justify-between">
+        <h2 className="text-[1.3rem] font-normal sm:text-[1.5rem] md:text-[2rem]">
+          Videos
+        </h2>
+        <Button
+          className="flex items-center gap-x-1 text-base font-medium leading-normal text-primary hover:bg-transparent hover:text-primary md:text-[1.2rem]"
+          variant="ghost"
+        >
+          See more
+          <ChevronRight size={25} />
+        </Button>
+      </div>
+      <div className="flex flex-col items-center justify-center gap-8 pt-8 lg:flex-row">
+        <div className="group relative h-full flex-1 cursor-pointer rounded-[0.25rem]">
+          <img
+            src={designerVideo1}
+            alt="designer-video-1"
+            className="h-full w-full rounded-[0.25rem] object-cover"
+          />
+          <div className="absolute inset-0 flex items-center justify-center rounded-[0.25rem] bg-black/30">
+            <div className="flex h-[4.25rem] w-[4.25rem] items-center justify-center rounded-full bg-primary group-hover:opacity-80">
+              <Play className="fill-white text-white" size={35} />
             </div>
           </div>
-          <div className="group relative h-full flex-1 cursor-pointer rounded-[0.25rem]">
-            <img
-              src={designerVideo2}
-              alt="designer-video-2"
-              className="h-full w-full rounded-[0.25rem] object-cover"
-            />
-            <div className="absolute inset-0 flex items-center justify-center rounded-[0.25rem] bg-black/30">
-              <div className="flex h-[4.25rem] w-[4.25rem] items-center justify-center rounded-full bg-primary group-hover:opacity-80">
-                <Play className="fill-white text-white" size={35} />
-              </div>
+        </div>
+        <div className="group relative h-full flex-1 cursor-pointer rounded-[0.25rem]">
+          <img
+            src={designerVideo2}
+            alt="designer-video-2"
+            className="h-full w-full rounded-[0.25rem] object-cover"
+          />
+          <div className="absolute inset-0 flex items-center justify-center rounded-[0.25rem] bg-black/30">
+            <div className="flex h-[4.25rem] w-[4.25rem] items-center justify-center rounded-full bg-primary group-hover:opacity-80">
+              <Play className="fill-white text-white" size={35} />
             </div>
           </div>
         </div>
