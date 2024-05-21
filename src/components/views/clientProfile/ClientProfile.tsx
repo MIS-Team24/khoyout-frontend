bc0a62340231058d966a780a10fe8f3d55091f31
AmrAbouElkair <amrmahmoud20022002@gmail.com>
2024-05-21 23:24:19 +0300

    Update ClientProfile component layout and styles

--- src/components/views/clientProfile/ClientProfile.tsx
+++ src/components/views/clientProfile/ClientProfile.tsx
@@ -1,14 +1,14 @@
 import PersonalInfoForm from "./PersonalInfoForm";
-import { esraa, edit, body, plusIcon } from "@/assets";
+import { esraa, edit, body } from "@/assets";
 import { Button } from "@/components/ui";
 import BodyMeasurmentform from "./BodyMeasurmentform";
-import { Link } from "@tanstack/react-router";
+import StylePreferences from "./StylePreferences";
 
 function ClientProfile() {
   return (
-    <section className="py-24">
-      <div className="mx-auto flex w-full max-w-[80rem] flex-col items-center justify-center">
-        <div className="relative mb-[7.4rem] flex h-[12.5rem] w-full items-end justify-between rounded-2xl bg-[#F3EBF1]">
+    <section>
+      <div className="main-container mb-16 mt-10">
+        <div className="relative mb-[6rem] flex h-[12.5rem] w-full items-end justify-between rounded-2xl bg-[#F3EBF1]">
           <div>
             <div className="absolute left-8 top-[6rem] h-[10rem] w-[10rem] rounded-full">
               <img
@@ -31,39 +31,25 @@ function ClientProfile() {
             </Button>
           </div>
         </div>
-        <div className="flex h-full w-full flex-col-reverse items-start justify-between gap-8 lg:flex-row">
-          <div className="flex w-full flex-col items-center gap-y-[1.06rem] lg:items-start ">
+        <div className="flex w-full flex-col-reverse items-start justify-between gap-5 md:flex-row">
+          <div className="flex w-full flex-col items-center gap-y-[1.06rem] lg:w-1/2 lg:items-start">
             <PersonalInfoForm />
             <BodyMeasurmentform />
-            <div className="flex max-h-[14rem] max-w-[39rem] flex-col gap-8 rounded-sm bg-[#F3EBF1] p-6">
-              <div className="gap-2">
-                <h2 className="h-[2.4rem] w-[19.1rem] text-[2rem] font-normal">
-                  My Style preferences
-                </h2>
-                <p className="h-12 w-[32.25rem] pt-4 text-base font-normal leading-6 text-[#6C6C6C]">
-                  By entering your style preferences, you'll help us match you
-                  with designers who specialize in your preferred styles
-                </p>
-              </div>
-              <Link className="flex w-full items-center justify-start gap-x-2 pb-4 text-xl leading-8 text-[#76526A]">
-                <img src={plusIcon} alt="Plus Icon" className="h-6 w-6" />
-                <span>Add your preferred style</span>
-              </Link>
-            </div>
+            <StylePreferences />
             <div className="flex w-full items-center justify-end">
               <Button
                 variant={"outline"}
-                className="w-[15.375rem] text-xl font-medium text-[#8C236C] hover:text-[#8C236C]"
+                className="w-full text-xl font-medium text-primary hover:text-primary max-md:h-12 md:w-[15.375rem]"
               >
                 Log out
               </Button>
             </div>
           </div>
-          <div className="mx-auto flex h-[30rem] w-[38rem] items-center justify-center rounded-2xl bg-[#F3EBF1] lg:h-[52.125rem] lg:w-full">
+          <div className="mx-auto h-full w-full rounded-xl bg-[#F3EBF1] lg:w-1/2">
             <img
               src={body}
               alt="Body Measurments Image"
-              className="h-full object-cover lg:h-[full] lg:w-[37rem]"
+              className="h-full w-full rounded-xl object-cover"
             />
           </div>
         </div>
