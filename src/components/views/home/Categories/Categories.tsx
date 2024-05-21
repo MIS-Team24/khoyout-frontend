fbc5fda718c17a12e2d818e0f61a127cf5d059a1
AmrAbouElkair <amrmahmoud20022002@gmail.com>
2024-05-21 21:29:47 +0300

    Refactor Categories component and update layout

--- src/components/views/home/Categories/Categories.tsx
+++ src/components/views/home/Categories/Categories.tsx
@@ -1,4 +1,5 @@
 import { categories } from "@/assets";
+import { SectionHeader } from "@/components/custom";
 import { motion } from "framer-motion";
 
 const spring = {
@@ -10,30 +11,24 @@ const spring = {
 
 function Categories() {
   return (
-    <section className="mt-16">
-      <div className="mx-auto flex w-full items-center justify-center">
-        <div className="flex max-w-[90rem] flex-col items-center text-center 2xl:text-start">
-          <h2 className="w-full pb-8 text-[1.3rem] font-normal sm:text-[1.5rem] md:text-[2rem]">
-            Browse Designers By Categories
-          </h2>
-          <div className="flex w-full flex-wrap justify-center gap-6 px-4 sm:px-0">
-            {categories.map(({ src, alt }, i) => (
-              <motion.div
-                initial={{ opacity: 0, x: i % 2 === 0 ? -50 : 50 }}
-                whileInView={{ opacity: 1, x: 0 }}
-                transition={spring}
-                className="h-[400px] w-full max-w-[708px] rounded-[0.25rem]"
-                key={i}
-              >
-                <img
-                  src={src}
-                  alt={alt}
-                  className="h-full w-full rounded-[0.5rem] object-cover"
-                />
-              </motion.div>
-            ))}
-          </div>
-        </div>
+    <section className="main-container mt-16">
+      <SectionHeader title="Browse Designers By Categories" className="mb-6" />
+      <div className="grid h-full w-full grid-cols-1 place-content-between gap-5 lg:grid-cols-2">
+        {categories.map(({ src, alt }, i) => (
+          <motion.div
+            initial={{ opacity: 0, x: i % 2 === 0 ? -50 : 50 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            transition={spring}
+            className="h-[400px] w-full rounded-[0.25rem]"
+            key={i}
+          >
+            <img
+              src={src}
+              alt={alt}
+              className="h-full w-full rounded-[0.5rem] object-cover"
+            />
+          </motion.div>
+        ))}
       </div>
     </section>
   );
