834f88a2d871d45b6fa8264672f3f4564f19c6f3
AmrAbouElkair <amrmahmoud20022002@gmail.com>
2024-05-30 15:01:56 +0300

    Update index.lazy.tsx to redirect from root to /home

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -1,6 +1,17 @@
-import { createLazyFileRoute } from "@tanstack/react-router";
+import { createLazyFileRoute, useNavigate } from "@tanstack/react-router";
 import Home from "../views/Home";
+import { useEffect } from "react";
 
 export const Route = createLazyFileRoute("/")({
-  component: Home,
+  component: Index,
 });
+
+function Index() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate({ from: "/", to: "/home" });
+  }, []);
+
+  return <Home />;
+}
