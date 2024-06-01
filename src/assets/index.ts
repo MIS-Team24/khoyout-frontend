b70beb84ab1697c0084577c7e2b4f00a38040568
AmrAbouElkair <amrmahmoud20022002@gmail.com>
2024-06-01 20:05:53 +0300

    Optimize asset imports and remove unused images

---
+++ src/assets/index.ts
@@ -0,0 +1,209 @@
+import sideImg from "./side-img.webp";
+import mainLogo from "./Logo.svg";
+import scratchImg from "./scratchImg.jpeg";
+import implementImg from "./implementImg.jpeg";
+import redesignImg from "./redesignImg.png";
+import handImg from "./handImg.jpeg";
+import quotes from "./Quotes.svg";
+import amira from "./amira.jpeg";
+import hager from "./hager.jpeg";
+import mai from "./may.jpeg";
+import heroImg from "./hero.png";
+import ins1 from "./Ins1.png";
+import ins2 from "./Ins2.png";
+import ins3 from "./Ins3.png";
+import ins4 from "./Ins4.png";
+import ins5 from "./Ins5.png";
+import ins6 from "./Ins6.png";
+import ins7 from "./Ins7.png";
+import aboutImg from "./about.png";
+import bookImg from "./book.png";
+import Ellipse from "./Ellipse 578.svg";
+import clientImg from "./clientImg.png";
+import designer1 from "./designer1.webp";
+import share from "./icons/share.svg";
+import heart from "./icons/heart.svg";
+import markerImg from "./map/marker.svg";
+import markerShadow from "./map/marker-shadow.png";
+import designerVideo1 from "./designer-video1.webp";
+import designerVideo2 from "./designer-video2.webp";
+import soiree from "./Soiree.jpeg";
+import formal from "./Formal.jpeg";
+import casual from "./Casual.jpeg";
+import classic from "./Classic.jpeg";
+import esraa from "./Esraa.png";
+import body from "./BodyImg.png";
+import edit from "./icons/Edit.svg";
+import email from "./icons/email.svg";
+import plusIcon from "./icons/plus-circle.svg";
+import circleDot from "./icons/circle-dot.png";
+import circleCheck from "./icons/circle-check.png";
+import circle from "./icons/circle.png";
+import design2 from "./Design2.png";
+import design5 from "./Design5.png";
+import design6 from "./Design6.png";
+import design8 from "./Design8.png";
+import team1 from "./team-1.png";
+import team2 from "./team-2.png";
+import team3 from "./team-3.png";
+
+const inspirationImages = [
+  {
+    src: ins1,
+    alt: "inspiration-image-1",
+  },
+  {
+    src: ins2,
+    alt: "inspiration-image-2",
+  },
+  {
+    src: ins3,
+    alt: "inspiration-image-3",
+  },
+  {
+    src: ins4,
+    alt: "inspiration-image-4",
+  },
+  {
+    src: ins5,
+    alt: "inspiration-image-5",
+  },
+  {
+    src: ins6,
+    alt: "inspiration-image-6",
+  },
+  {
+    src: ins7,
+    alt: "inspiration-image-7",
+  },
+];
+
+const workExamples = [
+  {
+    src: scratchImg,
+    alt: "Design from scratch",
+    title: "Design from scratch",
+  },
+  {
+    src: implementImg,
+    alt: "Design implementation",
+    title: "Design implementation",
+  },
+  {
+    src: redesignImg,
+    alt: "Redesign image",
+    title: "Redesign image",
+  },
+  {
+    src: handImg,
+    alt: "Hand made image",
+    title: "Hand made image",
+  },
+];
+
+const clients = [
+  {
+    src: amira,
+    alt: "Amira Ali",
+    name: "Amira Ali",
+    review:
+      "Khoyout made finding the perfect tailor a breeze! I was able to browse through various options, read reviews, and book my appointment with ease. Highly recommended!",
+  },
+  {
+    src: mai,
+    alt: "Mai Ahmed",
+    name: "Mai Ahmed",
+    review:
+      "Khoyout made finding the perfect tailor a breeze! I was able to browse through various options, read reviews, and book my appointment with ease. Highly recommended!",
+  },
+  {
+    src: hager,
+    alt: "Hager Ahmed",
+    name: "Hager Ahmed",
+    review:
+      "Khoyout made finding the perfect tailor a breeze! I was able to browse through various options, read reviews, and book my appointment with ease. Highly recommended!",
+  },
+];
+
+const categories = [
+  {
+    src: soiree,
+    alt: "Soiree Image",
+  },
+  {
+    src: classic,
+    alt: "Classic Image",
+  },
+  {
+    src: casual,
+    alt: "Casual Image",
+  },
+  {
+    src: formal,
+    alt: "Formal Image",
+  },
+];
+
+const designerGalleryImages = [
+  { src: design2, alt: "Design 1" },
+  { src: design5, alt: "Design 2" },
+  { src: design6, alt: "Design 3" },
+  { src: design8, alt: "Design 5" },
+  { src: ins1, alt: "Design 6" },
+  { src: ins2, alt: "Design 7" },
+  { src: ins3, alt: "Design 8" },
+  { src: ins4, alt: "Design 9" },
+  { src: ins5, alt: "Design 10" },
+  { src: ins6, alt: "Design 11" },
+  { src: ins7, alt: "Design 12" },
+];
+
+const team = [
+  {
+    image: { src: team1, alt: "team1 Image" },
+    name: "Nada",
+    job: "Hand made specialist",
+  },
+  {
+    image: { src: team2, alt: "team2 Image" },
+    name: "Mona",
+    job: "Redesign specialist",
+  },
+  {
+    image: { src: team3, atl: "team3 Image" },
+    name: "Mariam",
+    job: "Sewing specialist",
+  },
+];
+
+export {
+  sideImg,
+  mainLogo,
+  heroImg,
+  inspirationImages,
+  aboutImg,
+  bookImg,
+  workExamples,
+  quotes,
+  clients,
+  Ellipse,
+  clientImg,
+  designer1,
+  share,
+  heart,
+  markerImg,
+  markerShadow,
+  designerVideo1,
+  designerVideo2,
+  categories,
+  esraa,
+  body,
+  edit,
+  email,
+  plusIcon,
+  circleDot,
+  circle,
+  circleCheck,
+  designerGalleryImages,
+  team,
+};
