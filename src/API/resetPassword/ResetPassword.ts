7aa1aba03d8e8530bba04923a42a5e2040087eee
AmrAbouElkair <amrmahmoud20022002@gmail.com>
2024-06-01 21:48:06 +0300

    Update resetPassword API endpoint and request body structure

--- src/API/resetPassword/ResetPassword.ts
+++ src/API/resetPassword/ResetPassword.ts
@@ -1,12 +1,15 @@
 import client from "@/API/client";
 
 type API_ResetPasswordBody = {
-    password: string;
-    confirmPassword: string;
+  email: string;
+  password: string;
+  repeatPassword: string;
 };
 
 export function resetPassword(data: API_ResetPasswordBody) {
-    return client.post("/auth/reset-password", {
-        data,
-    });
+  console.log(data);
+
+  return client.put("/auth/reset-password", {
+    data,
+  });
 }
