7ade5e7471f5d108eafda23e29d740c0ebf8238d
AmrAbouElkair <amrmahmoud20022002@gmail.com>
2024-06-02 22:01:24 +0300

    Add API_DesignersResponse type for retrieving designers

---
+++ src/API/types/designers/designers.ts
@@ -0,0 +1,18 @@
+import { API_Pagination } from "../general/pagination";
+
+export type API_Designer = {
+  baseAccountId: string;
+  ordersFinished: number;
+  address: string;
+  yearsExperience: number;
+  rating: number;
+  avatarUrl: string;
+  name: string;
+  openNow: boolean;
+  openUntil: string;
+};
+
+export type API_DesignersResponse = {
+  designers: API_Designer[];
+  pagination: API_Pagination;
+};
