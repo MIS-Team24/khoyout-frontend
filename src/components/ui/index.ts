40a84f84c133fc5a75c34f42ecb3b6ffe7633750
AmrAbouElkair <amrmahmoud20022002@gmail.com>
2024-06-02 19:11:43 +0300

    Refactor dropdown menu component to export all sub-components

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -67,3 +67,21 @@ export {
   CardDescription,
   CardContent,
 } from "./card";
+
+export {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuCheckboxItem,
+  DropdownMenuRadioItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuGroup,
+  DropdownMenuPortal,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuRadioGroup,
+} from "./dropdown-menu";
