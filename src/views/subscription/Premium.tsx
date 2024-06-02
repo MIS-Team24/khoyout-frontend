14dabca52174a0fac28a965094ecc4c760a00755
ammarelshrief <amarelshrief511@gmail.com>
2024-06-03 00:54:35 +0300

    Add premiun component

---
+++ src/views/subscription/Premium.tsx
@@ -0,0 +1,14 @@
+import { PremiumPage } from "@/components/views/subscription";
+import { NavigatablePageWrapper } from "@/components/custom";
+
+function Premium() {
+  return (
+    <main>
+      <NavigatablePageWrapper>
+        <PremiumPage />
+      </NavigatablePageWrapper>
+    </main>
+  );
+}
+
+export default Premium;
