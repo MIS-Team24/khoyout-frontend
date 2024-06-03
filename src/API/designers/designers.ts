91c97944b284a9da33dd3dcecb12b48f4bd7b27a
AmrAbouElkair <amrmahmoud20022002@gmail.com>
2024-06-03 14:12:44 +0300

    Update getDesigners function to include additional query parameters

--- src/API/designers/designers.ts
+++ src/API/designers/designers.ts
@@ -1,5 +1,27 @@
 import client from "@/API/client";
 
-export function getDesigners() {
-  return client.get("/designer");
+export function getDesigners(
+  limit: number = 10,
+  page: number = 1,
+  name?: string,
+  openNow?: boolean,
+  sortBy?: string,
+  gender?: string,
+  yearsOfExperience?: number,
+  location?: string,
+  mainRating?: number,
+) {
+  return client.get("/designer", {
+    params: {
+      limit,
+      page,
+      name,
+      openNow,
+      sortBy,
+      gender,
+      yearsOfExperience,
+      location,
+      mainRating,
+    },
+  });
 }
