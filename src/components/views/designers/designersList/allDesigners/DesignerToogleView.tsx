efb1cc8a7123a72744ceeaaab7f45d0e4373c35e
AmrAbouElkair <amrmahmoud20022002@gmail.com>
2024-06-03 14:34:02 +0300

    Add DesignerToggleView component for toggling filters in designers list view

---
+++ src/components/views/designers/designersList/allDesigners/DesignerToogleView.tsx
@@ -0,0 +1,29 @@
+import { Button } from "@/components/ui";
+import { ListFilter } from "lucide-react";
+
+type DesignerToogleViewProps = {
+  handleToggle: () => void;
+};
+
+export default function DesignerToogleView({
+  handleToggle,
+}: DesignerToogleViewProps) {
+  return (
+    <div className="space-x-2">
+      <Button
+        variant="outline"
+        className="h-[3rem] w-[8rem] space-x-1 rounded-lg border text-lg font-medium text-primary hover:border-primary hover:text-primary focus:!ring-0 focus:!ring-offset-0 focus-visible:!ring-offset-0"
+        onClick={handleToggle}
+      >
+        <span className="pr-[0.5rem]">Filter</span>
+        <ListFilter className="h-5 w-5" />
+      </Button>
+      <Button
+        variant="ghost"
+        className="h-[3rem] w-[8rem] rounded-lg text-base font-medium text-gray-300 hover:bg-transparent hover:text-gray-500 focus:!ring-0 focus:!ring-offset-0 focus-visible:!ring-offset-0"
+      >
+        Clear filters
+      </Button>
+    </div>
+  );
+}
