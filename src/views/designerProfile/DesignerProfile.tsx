23ac529ed0e7087bc5e2d871aaaee588f5675984
AmrAbouElkair <amrmahmoud20022002@gmail.com>
2024-06-05 02:55:38 +0300

    Update designer profile component import

--- src/views/designerProfile/DesignerProfile.tsx
+++ src/views/designerProfile/DesignerProfile.tsx
@@ -1,43 +1,11 @@
-import { NavigatablePageWrapper, SectionHeader } from "@/components/custom";
-import {
-  ProfileDetails,
-  AboutDesigner,
-  DesignerVideos,
-  Service,
-} from "@/components/views/designerProfile";
-import { Inspiration } from "@/components/views/home";
-import { buttonVariants } from "@/components/ui";
-import { cn } from "@/lib/utils";
-import { Link } from "@tanstack/react-router";
-import { ChevronRight } from "lucide-react";
+import { NavigatablePageWrapper } from "@/components/custom";
+import { Designer } from "@/components/views/designerProfile";
 
 export default function DesignerProfile() {
   return (
     <NavigatablePageWrapper>
       {/* TODO: Make it dynamic */}
-      <ProfileDetails wishlisted={false} />
-      <AboutDesigner />
-      <Inspiration header={<Header />} />
-      <DesignerVideos />
-      <Service />
+      <Designer />
     </NavigatablePageWrapper>
-  );
-}
-
-function Header() {
-  return (
-    <div className="flex w-full items-center justify-between">
-      <SectionHeader className="my-0" title="Photos" />
-      <Link
-        to="/gallery/images"
-        className={cn(
-          buttonVariants({ variant: "ghost" }),
-          "flex items-center gap-x-1 text-base font-medium leading-normal text-primary hover:bg-transparent hover:text-primary md:text-[1.2rem]",
-        )}
-      >
-        See more
-        <ChevronRight size={25} />
-      </Link>
-    </div>
   );
 }
