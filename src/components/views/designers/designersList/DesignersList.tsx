9f6ae55d5c39dc43e3b72a1be208de4b9502297f
AmrAbouElkair <amrmahmoud20022002@gmail.com>
2024-06-05 01:40:23 +0300

    Refactor DesignersList component for improved code readability and UI consistency

--- src/components/views/designers/designersList/DesignersList.tsx
+++ src/components/views/designers/designersList/DesignersList.tsx
@@ -137,7 +137,7 @@ export default function DesignersList({ name }: DesignersListProps) {
 
 const DesignersSkeleton = () => {
   return (
-    <section className="relative right-0 w-full">
+    <section>
       <div className="relative grid grid-cols-1 gap-x-4 gap-y-8 md:grid-cols-2 lg:grid-cols-3">
         {Array.from({ length: 12 }).map((_, i) => (
           <div className="h-[540px] flex-1 rounded-lg" key={i}>
