7ffce2f4cf6f02d7213ec72df98eb715bacf26b5
AmrAbouElkair <amrmahmoud20022002@gmail.com>
2024-06-06 18:32:17 +0300

    Add user type selection to registration form

--- src/API/auth/register/register.ts
+++ src/API/auth/register/register.ts
@@ -1,17 +1,27 @@
 import client from "@/API/client";
+import { UserType } from "@/API/response_enums";
 
 type API_RegisterBody = {
   email: string;
   fullName: string;
   password: string;
   confirmPassword: string;
+  userType: UserType;
 };
 
 export function register(data: API_RegisterBody) {
-  return client.post("/auth/register", {
-    ...data,
-    repeatPassword: data.confirmPassword,
-  });
+  return client.post(
+    "/auth/register",
+    {
+      ...data,
+      repeatPassword: data.confirmPassword,
+    },
+    {
+      headers: {
+        "Account-Type": data.userType ?? UserType.USER,
+      },
+    },
+  );
 }
 
 type API_VerifyEmailBody = {
