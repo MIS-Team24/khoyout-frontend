19ad4fcbc3a42b23bc1b85ab33126f8967198f60
AmrAbouElkair <amrmahmoud20022002@gmail.com>
2024-06-08 19:44:05 +0300

    Add getDesignerPortfolio function to API designer module

--- src/API/designer/designer.ts
+++ src/API/designer/designer.ts
@@ -3,3 +3,14 @@ import client from "../client";
 export function getDesigner(id: string) {
   return client.get(`/designer/${id}`);
 }
+export function getDesignerPortfolio(id: string) {
+  return client.get(`/designer/${id}/portfolio`);
+}
+
+// getDesigner("108acf50-6e76-4a67-9c99-c8f434e2db96").then((res) => {
+//   console.log(res.data);
+// });
+
+// getDesignerPortfolio("108acf50-6e76-4a67-9c99-c8f434e2db96").then((res) => {
+//   console.log(res.data);
+// });
