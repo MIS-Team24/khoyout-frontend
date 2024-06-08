424dbd20c25e54313046a38d7bd823157947cf6a
AmrAbouElkair <amrmahmoud20022002@gmail.com>
2024-06-08 18:05:10 +0300

    Update Designers component grid layout to include xl breakpoint

--- src/components/views/designers/designersList/allDesigners/Designers.tsx
+++ src/components/views/designers/designersList/allDesigners/Designers.tsx
@@ -11,7 +11,7 @@ export default function Designers({ desigenrs }: DesignerProps) {
     <section className="w-full">
       <div
         className={cn(
-          "relative grid grid-cols-1 gap-x-4 gap-y-8 md:grid-cols-2 lg:grid-cols-3",
+          "relative grid grid-cols-1 gap-x-4 gap-y-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4",
         )}
       >
         {desigenrs?.map(
