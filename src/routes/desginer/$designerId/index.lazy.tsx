89e7e181cc616a6cb5feed68cd9213ed99a675ca
AmrAbouElkair <amrmahmoud20022002@gmail.com>
2024-06-08 18:04:58 +0300

    refactor: rename designer.lazy.tsx to index.lazy.tsx for consistency

--- src/routes/desginer/$designerId/index.lazy.tsx
+++ src/routes/desginer/$designerId/index.lazy.tsx
@@ -1,6 +1,6 @@
 import { createLazyFileRoute } from "@tanstack/react-router";
 import DesignerProfile from "@/views/designerProfile/DesignerProfile";
 
-export const Route = createLazyFileRoute("/$designerId/designer")({
+export const Route = createLazyFileRoute("/desginer/$designerId/")({
   component: DesignerProfile,
 });
