44cf6d1a3252b6bbcef9a7711939a3f5c6d5caac
AmrAbouElkair <amrmahmoud20022002@gmail.com>
2024-06-09 23:56:14 +0300

    Add appointments API functions for getting available times and booking appointments

---
+++ src/API/appointments/appointments.ts
@@ -0,0 +1,42 @@
+import client from "../client";
+
+export function getAvailableTimes(
+  access_token: string,
+  id: string,
+  timeZone: string,
+) {
+  return client.get(`/appointments/${id}/available-times`, {
+    params: {
+      "time zone": timeZone,
+    },
+    headers: {
+      Authorization: `Bearer ${access_token}`,
+    },
+  });
+}
+
+export function bookAppointment(
+  access_token: string,
+  id: string,
+  availableTimeId: number,
+  requestDescription: string,
+  date: string,
+  //   service: string,
+  //   img: string,
+) {
+  return client.post(
+    `/appointments/${id}/requests`,
+    {
+      availableTimeId,
+      requestDescription,
+      date,
+      //   service,
+      // img,
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+      },
+    },
+  );
+}
