253c3205928f0f4d4f4cfc2e469dfe63c405f39e
Ezz Ashraf AboAlhamed <73319030+EzzOps@users.noreply.github.com>
2024-06-09 06:26:11 +0300

    Update inspiration.lazy.tsx

--- src/routes/inspiration.lazy.tsx
+++ src/routes/inspiration.lazy.tsx
@@ -4,12 +4,13 @@ export const Route = createLazyFileRoute("/inspiration")({
   component: () => (
     <div>
       Hello /inspiration!
-      <script
-        type="module"
-        src="https://gradio.s3-us-west-2.amazonaws.com/4.24.0/gradio.js"
-      ></script>
-
-      <gradio-app src="https://yisol-idm-vton.hf.space"></gradio-app>
+      <div dangerouslySetInnerHTML={{ __html: `
+        <script
+          type="module"
+          src="https://gradio.s3-us-west-2.amazonaws.com/4.24.0/gradio.js"
+        ></script>
+        <gradio-app src="https://yisol-idm-vton.hf.space"></gradio-app>
+      ` }} />
     </div>
   ),
 });
