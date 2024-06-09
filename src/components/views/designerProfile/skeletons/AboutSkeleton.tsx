15b28e1b3788e42d6a82d8b1b691915568a6380c
AmrAbouElkair <amrmahmoud20022002@gmail.com>
2024-06-09 16:22:53 +0300

    Update skeleton components with improved styling and performance optimizations

--- src/components/views/designerProfile/skeletons/AboutSkeleton.tsx
+++ src/components/views/designerProfile/skeletons/AboutSkeleton.tsx
@@ -3,26 +3,26 @@ import { Map } from "lucide-react";
 
 const AboutSkeleton = () => (
   <section className="main-container">
-    <Skeleton className="mb-8 mt-16 h-[40px] w-[100px] rounded bg-gray-100" />
+    <Skeleton className="mb-8 mt-16 h-[40px] w-[100px] rounded bg-gray-300" />
     <div className="grid grid-cols-1 gap-x-10 gap-y-10 lg:grid-cols-2 lg:gap-y-0">
       <div className="flex flex-1 flex-col gap-y-[2.5rem]">
         <div className="space-y-2">
-          <Skeleton className="h-[28px] w-[710px] rounded bg-gray-100" />
-          <Skeleton className="h-[28px] w-[710px] rounded bg-gray-100" />
-          <Skeleton className="h-[28px] w-[710px] rounded bg-gray-100" />
+          <Skeleton className="h-[28px] w-[710px] rounded bg-gray-300" />
+          <Skeleton className="h-[28px] w-[710px] rounded bg-gray-300" />
+          <Skeleton className="h-[28px] w-[710px] rounded bg-gray-300" />
         </div>
         <div className="space-y-8">
-          <Skeleton className="h-[32px] w-[180px] rounded bg-gray-100" />
+          <Skeleton className="h-[32px] w-[180px] rounded bg-gray-300" />
           <div className="space-y-3.5">
             {Array(7)
               .fill(0)
               .map((_, i) => (
                 <div className="flex justify-between">
-                  <Skeleton className="h-[25px] w-[100px] rounded bg-gray-100" />
+                  <Skeleton className="h-[25px] w-[100px] rounded bg-gray-300" />
                   {i === 6 ? (
-                    <Skeleton className="h-[28px] w-[100px] rounded bg-gray-100" />
+                    <Skeleton className="h-[28px] w-[100px] rounded bg-gray-300" />
                   ) : (
-                    <Skeleton className="h-[28px] w-[192px] rounded bg-gray-100" />
+                    <Skeleton className="h-[28px] w-[192px] rounded bg-gray-300" />
                   )}
                 </div>
               ))}
@@ -31,15 +31,15 @@ const AboutSkeleton = () => (
       </div>
       <div className="flex h-[30rem] flex-1 flex-col justify-between gap-y-4 lg:h-full">
         <div className="relative flex h-full w-full items-center justify-center">
-          <Skeleton className="absolute inset-0 rounded bg-gray-100" />
-          <Map size={45} className="animate-pulse text-gray-300" />
+          <Skeleton className="absolute inset-0 rounded bg-gray-300" />
+          <Map size={45} className="animate-pulse text-gray-500" />
         </div>
         <div className="mt-auto flex w-full flex-col items-center justify-between gap-y-4 sm:flex-row sm:gap-y-0">
           <div className="h-full flex-1">
-            <Skeleton className="h-[28px] w-[calc(100%-80px)] rounded bg-gray-100" />
+            <Skeleton className="h-[28px] w-[calc(100%-80px)] rounded bg-gray-300" />
           </div>
           <div className="flex h-full flex-1 justify-end">
-            <Skeleton className="h-[28px] w-[calc(100%-80px)] rounded bg-gray-100" />
+            <Skeleton className="h-[28px] w-[calc(100%-80px)] rounded bg-gray-300" />
           </div>
         </div>
       </div>
--- src/components/views/designerProfile/skeletons/ServiceSkeleton.tsx
+++ src/components/views/designerProfile/skeletons/ServiceSkeleton.tsx
@@ -21,7 +21,7 @@ const ServiceSkeleton = ({
   serviceLength,
 }: ServiceSkeletonProps) => (
   <div className="main-container">
-    <Skeleton className="mb-8 mt-16 h-[40px] w-[100px] rounded bg-gray-100" />
+    <Skeleton className="mb-8 mt-16 h-[40px] w-[100px] rounded bg-gray-300" />
     <div className="mb-8 flex flex-col place-items-start gap-8 pb-0 text-xl lg:flex-row">
       <div className="mx-auto flex w-full flex-col gap-[4.25rem] lg:w-3/5">
         <section>
@@ -33,15 +33,15 @@ const ServiceSkeleton = ({
                   <Card className="rounded-2xl" key={`service-${i}`}>
                     <CardHeader className="gap-4">
                       <CardTitle className="font-medium text-foreground">
-                        <Skeleton className="h-[28px] w-[200px] rounded bg-gray-100" />
+                        <Skeleton className="h-[28px] w-[200px] rounded bg-gray-300" />
                       </CardTitle>
                       <CardDescription className="text-xl text-secondary">
-                        <Skeleton className="h-[28px] w-full rounded bg-gray-100" />
+                        <Skeleton className="h-[28px] w-full rounded bg-gray-300" />
                       </CardDescription>
                     </CardHeader>
                     <CardFooter className="flex place-items-center justify-between">
-                      <Skeleton className="h-[30px] w-[100px] rounded bg-gray-100" />
-                      <Skeleton className="h-[35px] w-[80px] rounded bg-gray-100" />
+                      <Skeleton className="h-[30px] w-[100px] rounded bg-gray-300" />
+                      <Skeleton className="h-[35px] w-[80px] rounded bg-gray-300" />
                     </CardFooter>
                   </Card>
                 ))}
@@ -63,7 +63,7 @@ export default ServiceSkeleton;
 const TeamSkeleton = ({ membersLength }: { membersLength: number }) => (
   <section>
     <div>
-      <Skeleton className="mb-8 mt-16 h-[40px] w-[100px] rounded bg-gray-100" />
+      <Skeleton className="mb-8 mt-16 h-[40px] w-[100px] rounded bg-gray-300" />
       <div className="no-scrollbar flex flex-row gap-6 overflow-auto whitespace-nowrap">
         {Array.from({ length: membersLength })
           .fill(0)
@@ -71,12 +71,12 @@ const TeamSkeleton = ({ membersLength }: { membersLength: number }) => (
             <Card className="min-w-48 p-2" key={`team-member-${i}`}>
               <CardContent className="p-0 text-center">
                 <div className="relative flex h-44 w-44 items-center justify-center rounded">
-                  <Skeleton className="absolute h-full w-full rounded bg-gray-100" />
-                  <Image size={50} className="animate-pulse text-gray-300" />
+                  <Skeleton className="absolute h-full w-full rounded bg-gray-300" />
+                  <Image size={50} className="animate-pulse text-gray-400" />
                 </div>
                 <div className="mt-6 flex flex-col items-center">
-                  <Skeleton className="mb-2 h-[28px] w-[100px] rounded bg-gray-100" />
-                  <Skeleton className="h-[15px] w-[140px] rounded bg-gray-100" />
+                  <Skeleton className="mb-2 h-[28px] w-[100px] rounded bg-gray-300" />
+                  <Skeleton className="h-[15px] w-[140px] rounded bg-gray-300" />
                 </div>
               </CardContent>
             </Card>
@@ -90,11 +90,11 @@ const ReviewsSkeleton = ({ reviewsLength }: { reviewsLength: number }) => (
   <section className="lg:mb-[4.25rem]">
     <div>
       <div className="mb-8">
-        <Skeleton className="mb-8 h-[40px] w-[100px] rounded bg-gray-100" />
-        <Skeleton className="h-[40px] w-[180px] rounded bg-gray-100" />
+        <Skeleton className="mb-8 h-[40px] w-[100px] rounded bg-gray-300" />
+        <Skeleton className="h-[40px] w-[180px] rounded bg-gray-300" />
         <div className="mt-8 flex gap-4">
-          <Skeleton className="h-[40px] w-[60px] rounded bg-gray-100" />
-          <Skeleton className="h-[40px] w-[80px] rounded bg-gray-100" />
+          <Skeleton className="h-[40px] w-[60px] rounded bg-gray-300" />
+          <Skeleton className="h-[40px] w-[80px] rounded bg-gray-300" />
         </div>
       </div>
       <div>
@@ -107,17 +107,17 @@ const ReviewsSkeleton = ({ reviewsLength }: { reviewsLength: number }) => (
             >
               <div className="flex gap-2">
                 <div>
-                  <Skeleton className="h-12 w-12 rounded-full bg-gray-100" />
+                  <Skeleton className="h-12 w-12 rounded-full bg-gray-300" />
                 </div>
                 <div className="space-y-2">
-                  <Skeleton className="h-[28px] w-[80px] rounded bg-gray-100" />
-                  <Skeleton className="h-[50px] w-[60px] rounded bg-gray-100" />
-                  <Skeleton className="h-[50px] w-[80px] rounded bg-gray-100" />
+                  <Skeleton className="h-[28px] w-[80px] rounded bg-gray-300" />
+                  <Skeleton className="h-[20px] w-[60px] rounded bg-gray-300" />
+                  <Skeleton className="h-[30px] w-[80px] rounded bg-gray-300" />
                 </div>
               </div>
               <div className="space-y-2">
-                <Skeleton className="h-[28px] w-full rounded bg-gray-100" />
-                <Skeleton className="h-[28px] w-full rounded bg-gray-100" />
+                <Skeleton className="h-[28px] w-full rounded bg-gray-300" />
+                <Skeleton className="h-[28px] w-full rounded bg-gray-300" />
               </div>
             </div>
           ))}
@@ -131,26 +131,26 @@ const BookingCardSkeleton = () => (
     <Card className="px-8 py-10">
       <CardHeader className="p-0">
         <CardTitle className="text-[2.5rem] text-foreground">
-          <Skeleton className="h-[80px] w-[200px] rounded bg-gray-100" />
+          <Skeleton className="h-[80px] w-[200px] rounded bg-gray-300" />
         </CardTitle>
       </CardHeader>
       <CardContent className="p-0">
         <div className="my-8 flex place-items-center gap-4">
-          <Skeleton className="h-[32px] w-[33px] rounded bg-gray-100" />
-          <Skeleton className="h-[32px] w-[180px] rounded bg-gray-100" />
-          <Skeleton className="h-[32px] w-[40px] rounded bg-gray-100" />
+          <Skeleton className="h-[32px] w-[33px] rounded bg-gray-300" />
+          <Skeleton className="h-[32px] w-[180px] rounded bg-gray-300" />
+          <Skeleton className="h-[32px] w-[40px] rounded bg-gray-300" />
         </div>
         <div className="mb-6 flex gap-2">
-          <Clock className="animate-pulse text-gray-200" />
-          <Skeleton className="h-[28px] w-[377px] rounded bg-gray-100" />
+          <Clock className="animate-pulse text-gray-300" />
+          <Skeleton className="h-[28px] w-[377px] rounded bg-gray-300" />
         </div>
         <div className="mb-6 flex gap-2">
-          <Clock className="animate-pulse text-gray-200" />
-          <Skeleton className="h-[28px] w-[377px] rounded bg-gray-100" />
+          <Clock className="animate-pulse text-gray-300" />
+          <Skeleton className="h-[28px] w-[377px] rounded bg-gray-300" />
         </div>
       </CardContent>
       <CardFooter className="p-0">
-        <Skeleton className="h-14 w-full rounded-2xl bg-gray-100" />
+        <Skeleton className="h-14 w-full rounded-2xl bg-gray-300" />
       </CardFooter>
     </Card>
   </div>
