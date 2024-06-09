49aa1ce0c05b15375cda9aefaf85d6b63cbb8f3a
Ezz Ashraf AboAlhamed <73319030+EzzOps@users.noreply.github.com>
2024-06-09 06:23:38 +0300

    Update inspiration.lazy.tsx

--- src/routes/inspiration.lazy.tsx
+++ src/routes/inspiration.lazy.tsx
@@ -3,7 +3,7 @@ import { createLazyFileRoute } from "@tanstack/react-router";
 export const Route = createLazyFileRoute("/inspiration")({
   component: () => (
     <div>
-      Hello /Model!
+      Hello /inspiration!
       <script
         type="module"
         src="https://gradio.s3-us-west-2.amazonaws.com/4.24.0/gradio.js"
