563d8b2614f3dd81dd089a90a209d00079867f57
AmrAbouElkair <amrmahmoud20022002@gmail.com>
2024-06-11 17:23:47 +0300

    Refactor ComingAppointment component to conditionally render DesignerAppointmentsPage based on user type

--- src/views/comingAppointment/ComingAppointment.tsx
+++ src/views/comingAppointment/ComingAppointment.tsx
@@ -1,14 +1,17 @@
 import ComingAppointmentPage from "@/components/views/comingAppointment/ComingAppointmentPage.tsx";
 import DesignerAppointmentsPage from "@/components/views/designerAppointment/DesignerAppointmentsPage.tsx";
-import {NavigatablePageWrapper} from "@/components/custom";
+import { NavigatablePageWrapper } from "@/components/custom";
+import useAuth from "@/hooks/useAuth";
 
-export default function comingAppointment() {
-    return (
-        <main>
-            <NavigatablePageWrapper>
-                <ComingAppointmentPage/>
-                <DesignerAppointmentsPage/>
-            </NavigatablePageWrapper>
-        </main>
-    );
+export default function ComingAppointment() {
+  const data = useAuth();
+  const isDesigner = data.auth.user?.user.type === "designer";
+
+  return (
+    <main>
+      <NavigatablePageWrapper>
+        {isDesigner ? <DesignerAppointmentsPage /> : <ComingAppointmentPage />}
+      </NavigatablePageWrapper>
+    </main>
+  );
 }
